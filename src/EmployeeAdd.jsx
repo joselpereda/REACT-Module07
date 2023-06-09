90b080eefbf5e5a02b100ece81033c786b8872b5
Jose Pereda <joselpereda@hotmail.com>
2023-06-09 05:12:11 -0700

    adding modal to employee add class

--- src/EmployeeAdd.jsx
+++ src/EmployeeAdd.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Container, Row, Col, Modal } from 'react-bootstrap'
+import { LoaderTargetPlugin } from 'webpack'
 
 export default class EmployeeAdd extends React.Component {
     constructor() {
         super()
+        this.State = {
+            modalVisible: false,
+        }
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleShowModal = this.handleShowModal.bind(this)
+        this.handleHideModal = this.handleHideModal.bind(this)
     }
+
+    handleShowModal() {
+        this.setState = ({modalVisible: true,})
+    }
+
+    handleHideModal() {
+
+    }
+
     handleSubmit(e) {
         e.preventDefault()
         const form = document.forms.employeeAdd
@@ -25,13 +40,34 @@ export default class EmployeeAdd extends React.Component {
     }
     render() {
         return(
-            <form name="employeeAdd" onSubmit={this.handleSubmit}>
-                Name: <input type="text" name="name" /><br/>
-                Extension: <input type="text" name="ext" /><br/>
-                Email: <input type="text" name="email" /><br/>
-                Title: <input type="text" name="title" /><br/>
-                <Button type="submit" variant="primary" size='sm'>Add Employee</Button>
-            </form>
+            <>
+                <div>
+                    <Button variant='primary' size='sm' onClick={this.handleShowModal}>
+                        New Employee
+                    </Button> 
+                </div>
+                <Container fluid>
+                    <form name="employeeAdd" onSubmit={this.handleSubmit}>
+                        <Row>
+                            <Col md={3}>Name: </Col>
+                            <Col md="auto"><input type="text" name="name" /></Col>
+                        </Row>
+                        <Row>
+                            <Col md={3}>Extension:</Col> 
+                            <Col md="auto"><input type="text" name="ext" maxLength={4}/></Col>
+                        </Row>
+                        <Row>
+                            <Col md={3}>Email:</Col> 
+                            <Col md="auto"><input type="text" name="email" /></Col>
+                        </Row>
+                        <Row>
+                            <Col md={3}>Title: </Col> 
+                            <Col md="auto"><input type="text" name="title" /></Col>
+                            <Button type="submit" variant="primary" size='sm'>Add Employee</Button>
+                        </Row>
+                    </form>
+                </Container>
+            </>
         )
     } 
 }
